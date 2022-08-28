56fb05479c66f49345b53b5a061d603de1a7710f
vishnu9651 <vishnukantee0044@gmail.com>
2022-08-28 06:32:41 +0530

    products

--- src/Category/Appliances.jsx
+++ src/Category/Appliances.jsx
@@ -1,7 +1,9 @@
+import { IconName } from "react-icons/go";
+
 function Category(){
 return (
 <div style={{border:"1px solid red", width:"50px"}}>
-<i class="fa-solid fa-box-archive"></i>
+
 </div>
 
 )
